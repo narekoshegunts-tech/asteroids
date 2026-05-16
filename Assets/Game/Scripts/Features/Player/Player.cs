1f493145990e37d2dfce32b1c06dc773cdf7e018
Narek <narench@inbox.ru>
2026-05-16 15:12:44 +0300

    + update player collision detector

--- Assets/Game/Scripts/Features/Player/Player.cs
+++ Assets/Game/Scripts/Features/Player/Player.cs
@@ -1,12 +1,23 @@
-﻿using UnityEngine;
+﻿using Game.Scripts.Features.Enemies;
+using UnityEngine;
 
 namespace Game.Scripts.Features.Player
 {
     public class Player: MonoBehaviour
     {
-        public void GetDamage()
+        private void GetDamage()
         {
             Debug.Log("GetDamage");
+        }
+        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<Enemy>(out var enemy))
+            {
+                enemy.Destroy();
+            }
+
+            GetDamage();
         }
     }
 }
