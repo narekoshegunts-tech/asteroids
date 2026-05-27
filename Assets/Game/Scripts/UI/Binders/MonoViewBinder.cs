87e1611f790b0256bd9f7081049ebcdd1f761af1
Narek <narench@inbox.ru>
2026-05-27 21:17:21 +0300

    + debug tools for android

--- Assets/Game/Scripts/UI/Binders/MonoViewBinder.cs
+++ Assets/Game/Scripts/UI/Binders/MonoViewBinder.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using MVVM;
-using UnityEditor;
 using UnityEngine;
 using Zenject;
 using Object = UnityEngine.Object;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Game.Scripts.UI.Binders
 {
@@ -18,26 +21,36 @@ namespace Game.Scripts.UI.Binders
 
         [SerializeField]
         private BindingMode viewBinding;
-        
+
         [SerializeField]
         private Object view;
         
+#if UNITY_EDITOR
         [SerializeField]
         private MonoScript viewType;
+#endif
         
+        [SerializeField, HideInInspector]
+        private string viewTypeFullName;
+
         [SerializeField]
         private string viewId;
 
         [Space(8)]
         [SerializeField]
         private BindingMode viewModelBinding;
-        
+
         [SerializeField]
         private Object viewModel;
-        
+
+#if UNITY_EDITOR
         [SerializeField]
         private MonoScript viewModelType;
-        
+#endif
+
+        [SerializeField, HideInInspector]
+        private string viewModelTypeFullName;
+
         [SerializeField]
         private string viewModelId;
 
@@ -48,38 +61,74 @@ namespace Game.Scripts.UI.Binders
 
         private void Awake()
         {
-            _binder = this.CreateBinder();
+            _binder = CreateBinder();
         }
 
         private void OnEnable()
         {
-            _binder.Bind();
+            _binder?.Bind();
         }
 
         private void OnDisable()
         {
-            _binder.Unbind();
+            _binder?.Unbind();
         }
 
         private IBinder CreateBinder()
         {
-            object view = this.viewBinding switch
+            object viewObj = viewBinding switch
             {
-                BindingMode.FromInstance => this.view,
-                BindingMode.FromResolve => this.diContainer.Resolve(this.viewType.GetClass()),
-                BindingMode.FromResolveId => this.diContainer.ResolveId(this.viewType.GetClass(), this.viewId),
-                _ => throw new Exception($"Binding type of view {this.viewBinding} is not found!")
+                BindingMode.FromInstance => view,
+                BindingMode.FromResolve => ResolveType(viewTypeFullName),
+                BindingMode.FromResolveId => ResolveTypeWithId(viewTypeFullName, viewId),
+                _ => throw new Exception($"Unknown view binding mode: {viewBinding}")
             };
 
-            object model = this.viewModelBinding switch
+            object modelObj = viewModelBinding switch
             {
-                BindingMode.FromInstance => this.viewModel,
-                BindingMode.FromResolve => this.diContainer.Resolve(this.viewModelType.GetClass()),
-                BindingMode.FromResolveId => this.diContainer.ResolveId(this.viewModelType.GetClass(), this.viewModelId),
-                _ => throw new Exception($"Binding type of view {this.viewBinding} is not found!")
+                BindingMode.FromInstance => viewModel,
+                BindingMode.FromResolve => ResolveType(viewModelTypeFullName),
+                BindingMode.FromResolveId => ResolveTypeWithId(viewModelTypeFullName, viewModelId),
+                _ => throw new Exception($"Unknown viewModel binding mode: {viewModelBinding}")
             };
 
-            return BinderFactory.CreateComposite(view, model);
+            return BinderFactory.CreateComposite(viewObj, modelObj);
         }
+
+        private object ResolveType(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+                throw new Exception("Type full name is empty");
+
+            var type = Type.GetType(fullName);
+            if (type == null)
+                throw new Exception($"Cannot resolve type: {fullName}");
+
+            return diContainer.Resolve(type);
+        }
+
+        private object ResolveTypeWithId(string fullName, string id)
+        {
+            if (string.IsNullOrEmpty(fullName))
+                throw new Exception("Type full name is empty");
+
+            var type = Type.GetType(fullName);
+            if (type == null)
+                throw new Exception($"Cannot resolve type: {fullName}");
+
+            return diContainer.ResolveId(type, id);
+        }
+
+        // ==================== Editor Helper ====================
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (viewType != null)
+                viewTypeFullName = viewType.GetClass()?.AssemblyQualifiedName;
+
+            if (viewModelType != null)
+                viewModelTypeFullName = viewModelType.GetClass()?.AssemblyQualifiedName;
+        }
+#endif
     }
 }
